d4a1cd346cd08d1a9c07170352741d3fd6f338a1
hjc59476905 <89001850+hjc59476905@users.noreply.github.com>
2023-02-02 02:09:45 +0800

    feat：添加基础卡片组件

---
+++ src/components/card/DefCard/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="def-card">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, reactive } from 'vue'
+onMounted(() => {
+
+})
+</script>
+
+<style lang='scss' scoped>
+.def-card {
+  border-radius: 10px;
+  background: var(--card-color);
+  transition: background 0.8s;
+
+  &:hover {
+    box-shadow: 0 0 20px var(--float-color);
+    transition: box-shadow 0.3s;
+  }
+}
+</style>
