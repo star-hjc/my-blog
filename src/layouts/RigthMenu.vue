500812bc60b1247ee5ba2074061b998fa4f2707a
hjc59476905 <89001850+hjc59476905@users.noreply.github.com>
2023-02-02 02:11:14 +0800

    feat：添加右下角菜单组件

---
+++ src/layouts/RigthMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="rigth-menu">
+    <el-drawer v-model="isShowRigthMenu" direction="rtl" size="60%">
+      <template #default>
+      <div>
+        12221
+      </div>
+    </template>
+    </el-drawer>
+  </div>
+
+</template>
+
+<script setup>
+const isShowRigthMenu = ref(false)
+
+const onShowRigthMenu = () => {
+  isShowRigthMenu.value = !isShowRigthMenu.value
+}
+
+defineExpose({ onShowRigthMenu })
+</script>
+
+<style lang='scss' scoped>
+.rigth-menu {
+  color: black;
+
+  & :deep(.el-drawer__header) {
+    display: none;
+  }
+
+  & :deep(.el-drawer__body) {
+    padding: 0;
+  }
+}
+</style>
