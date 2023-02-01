fa076cd77f5c2e36f3f982b7365e84aa1acf0e1a
hjc59476905 <89001850+hjc59476905@users.noreply.github.com>
2023-02-02 01:57:56 +0800

    feat：添加 rollup-plugin-visualizer 包
    打包文件大小分析

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,20 @@ import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+/** build 视图分析 */
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+        visualizer({
+            emitFile: false,
+            file: 'build.html',
+            open: true
+        }),
         AutoImport({
+            imports: ['vue'],
             resolvers: [ElementPlusResolver()]
         }),
         Components({
@@ -30,11 +38,39 @@ export default defineConfig({
         }
     },
     server: {
+        /** 启动打开网页 */
+        open: true,
+        /** 开启热更新 */
+        hmr: true,
+        // host:'0.0.0.0',
         /** 跨域配置 */
         proxy: {
             '/api': {
                 target: 'http://120.77.79.68:8888/',
                 changeOrigin: true,
+            }
+        }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(buildFilePath) {
+                    // if (nodeModules) {
+                    //     // console.log(buildFilePath.split('node_modules/')[1].split('/')[0]);
+                    //     return buildFilePath.split('node_modules/')[1].split('/')[0];
+                    // }
+                    if (buildFilePath.indexOf('node_modules') === -1 && buildFilePath.includes('components/card/')) {
+                        console.log(String(buildFilePath));
+                        return buildFilePath.split('components/card/')[1].split('.')[0]
+                    }
+                    // if ( nodeModules && buildFilePath.includes('element-plus/')) {
+                    //     console.log(buildFilePath.split('element-plus/')[1].split('/')[0]);
+                    // }
+
+                    function pathIsExist(name) {
+                        return buildFilePath.indexOf(name) !== -1
+                    }
+                }
             }
         }
     }
