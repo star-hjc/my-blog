e02908776a725bb9030686c2a05f8ac1ef9e9693
hjc59476905 <89001850+hjc59476905@users.noreply.github.com>
2023-02-02 02:10:20 +0800

    feat：添加基础玻璃卡片组件

---
+++ src/components/card/GlassCard/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <div ref="cardDOM" class="glass-card" :style="{ height, width }">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+import VanillaTilt from 'vanilla-tilt'
+import { onMounted, ref } from 'vue'
+const cardDOM = ref(null)
+
+const props = defineProps({
+    height: {
+        type: String,
+        default: 'infinite'
+    },
+    width: {
+        type: String,
+        default: 'infinite'
+    },
+    isAction: {
+        type: Boolean,
+        default: true
+    }
+})
+
+onMounted(() => {
+    if (props.isAction) {
+        VanillaTilt.init(cardDOM.value, {
+            max: 25,
+            speed: 400,
+            glare: true
+        })
+    }
+})
+
+
+
+</script>
+
+<style lang="scss" scoped>
+.glass-card {
+    height: infinite;
+    width: infinite;
+    background: rgba(255, 255, 255, 0.1);
+    border-radius: 10px;
+    border-top: 1px solid #FFF;
+    border-left: 1px solid #FFF;
+    overflow: hidden;
+    box-shadow: 8px 8px 12px rgba(0, 0, 0, 0.5);
+}
+</style>
