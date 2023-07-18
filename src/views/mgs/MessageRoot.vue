098b215b2daac9cb16e67314a955cac54925ab97
租客的鱼 <2996731483@qq.com>
2023-07-18 19:14:57 +0800

    聊天框UI完善

--- src/views/mgs/MessageRoot.vue
+++ src/views/mgs/MessageRoot.vue
@@ -5,27 +5,16 @@
     <div class="message-root">
         <div class="content">
             <div class="top">聊天界面</div>
-            <div class="content">
-                <el-scrollbar height="400px">
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
-                    <p>内容</p>
+            <div class="chat">
+                <el-scrollbar height="70vh">
+                    <ul class="talk_list">
+                        <li  v-for="(item,index) in contentlist" :key="index" :class="item.direction === 0? 'content-left': 'content-right'"><img :src="item.pisrc"><span>{{ item.content }}</span></li>
+                    </ul>
                 </el-scrollbar>
             </div>
             <div class="footer">
-                <el-input v-model="input" placeholder="请输入聊天内容" />
+                <el-input v-model="input"  placeholder="请输入聊天内容" ref="ipref" class="inputel"  clearable/>
+                <el-button @click="send">发送</el-button>
             </div>
         </div>
     </div>
@@ -35,10 +24,44 @@
 <script setup>
 import { ref } from 'vue'
 import TopBar from '@/layouts/TopBar.vue'
-onMounted(() => {
-
-})
+const contentlist = ref([
+    {
+        id: 594203,
+        direction: 0, // 0 代表别人的信息 1 代表自己的
+        pisrc: '/src/picture/person01.png',
+        content: '你好呀！'
+    },
+    {
+        id: 594204,
+        direction: 1,
+        pisrc: '/src/picture/person02.png',
+        content: '我很好'
+    },
+    {
+        id: 594205,
+        direction: 0,
+        pisrc: '/src/picture/person01.png',
+        content: '最近怎么样'
+    },
+    {
+        id: 594205,
+        direction: 1,
+        pisrc: '/src/picture/person01.png',
+        content: '最近好的不得了'
+    }
+])
 const input = ref()
+const send = () => {
+    contentlist.value.push(
+        {
+            id: 594206,
+            direction: 1, // 0 代表别人的信息 1 代表自己的
+            pisrc: '/src/picture/person02.png',
+            content: input.value
+        }
+    )
+    input.value = ''
+}
 </script>
 
 <style lang="scss" scoped>
@@ -53,24 +76,54 @@ const input = ref()
     box-sizing: border-box;
 
     .content {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        height: auto;
-        width: auto;
         background-color: var(--card-color);
         color: black;
         border-radius: 10px;
         color: var(--font-color);
 
+        .chat {
+            ul {
+                padding-left: 0px;
+                font-weight: 600;
+
+                .content-left {
+                    margin-bottom: 10px;
+
+                    span {
+                        padding: 10px;
+                        background: #fe9697;
+                        border-radius: 5px;
+                        margin-left: 10px;
+                    }
+                }
+
+                .content-right {
+                    margin-top: 18px;
+                    display: flex;
+                    flex-flow: row-reverse;
+
+                    span {
+                        padding: 10px;
+                        background: #409eff;
+                        border-radius: 5px;
+                        margin-right: 10px;
+                        float: left;
+
+                    }
+                }
+            }
+        }
+
         .top {
             height: 50px;
             line-height: 50px;
             text-align: center;
-            background: green;
+            background: linear-gradient(90deg, rgba(206, 43, 40, 0.756), rgba(0, 42, 254, 0.6));
+            color: azure;
         }
-        .ffooter {
-            height: 30px;
+
+        .footer {
+            display: flex;
         }
     }
 }
